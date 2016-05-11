f47af6b7d251fa49b662135de513fc255b4d53c5
appdesign1987 <jeroen@jeroenvd.nl>
2016-05-11 19:52:35 +0200

    Update functions.php

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -699,9 +699,9 @@ function makeWeatherSidebar()
 	//	echo '<h4 class="exoextralight" style="margin-top:0px">Wind: '.$currentWindSpeed.' mph ('.$direction.')</h4>';
 	//}
 	//echo '<h4 class="exoregular">Next Hour</h4>';
-	echo '<h5 class="exoextralight" style="margin-top:10px">'.$minutelySummary.'</h5>';
+	//echo '<h5 class="exoextralight" style="margin-top:10px">'.$minutelySummary.'</h5>';
 	//echo '<h4 class="exoregular">Next 24 Hours</h4>';
-	//cho '<h5 class="exoextralight" style="margin-top:10px">'.$hourlySummary.'</h5>';
+	echo '<h5 class="exoextralight" style="margin-top:10px">'.$hourlySummary.'</h5>';
 	echo '<p class="text-right no-link-color"><small><a href="http://forecast.io/#/f/',$forecastLat,',',$forecastLong,'">Forecast.io</a></small></p>';
 }
 
