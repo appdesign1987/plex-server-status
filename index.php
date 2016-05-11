c28c589ac3eb61c485e18bfa36517677c70da08a
appdesign1987 <jeroen@jeroenvd.nl>
2016-05-11 21:14:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -103,10 +103,7 @@
 						$('#system_load').show();
 						$('#disk_space').show();
 						$('#other_disks').show();
-						//$('#now_playing_title').show();
-						//$('#now_playing').show();
 						$('#system_ram').show();
-						//$('#plex_movie_stats').show();
 		            		},
 				            complete: function() {
 				            	$('#left_column_top').show();
@@ -129,15 +126,11 @@
 						$('#system_load').show();
 						$('#disk_space').show();
 						$('#other_disks').show();
-						//$('#now_playing_title').show();
-						//$('#now_playing').show();
 						$('#system_ram').show();
-						//$('#plex_movie_stats').show();
 				            }
 				});
 
 				// Assign varibles to DOM sections
-				//var $plex_check_refresh = $('#plex_check');
 				var $left_column_top_refresh = $('#left_column_top');
 				var $bandwidth_refresh = $('#bandwidth');
 				var $ping_refresh = $('#ping');
@@ -145,29 +138,18 @@
 			        	var $system_load_refresh = $('#system_load');
 			        	var $disk_space_refresh = $('#disk_space');
 			        	var $other_disks_refresh = $('#other_disks');
-			        //	var $now_playing_title_refresh = $('#now_playing_title');
-			        //	var $now_playing_refresh = $('#now_playing');
 			        	var $system_ram_refresh = $('#system_ram');
-			        //	var $plex_movie_stats_refresh = $('#plex_movie_stats');
 
 			        	// Load external php files & assign variables
-			        //	$plex_check_refresh.load('assets/php/plex_check_ajax.php');
 			        	$left_column_top_refresh.load('assets/php/left_column_top_ajax.php');
 			        	$bandwidth_refresh.load("assets/php/bandwidth_ajax.php");
 			        	$ping_refresh.load("assets/php/ping_ajax.php");
 			        	$services_refresh.load("assets/php/services_ajax.php");
 			        	$system_load_refresh.load("assets/php/system_load_ajax.php");
 			        	$disk_space_refresh.load("assets/php/disk_space_ajax.php");
-			        //	$other_disks_refresh.load("assets/php/other_disks_ajax.php");
-			        //	$now_playing_title_refresh.load("assets/php/now_playing_title_ajax.php");
-			        //	$now_playing_refresh.load("assets/php/now_playing_ajax.php");
-			        	$system_ram_refresh.load("assets/php/system_ram_ajax.php");
-			        //	$plex_movie_stats_refresh.load("assets/php/plex_movie_stats_ajax.php");
+			        	$other_disks_refresh.load("assets/php/other_disks_ajax.php");
+			      		$system_ram_refresh.load("assets/php/system_ram_ajax.php");
 			        
-				//var refreshIdfastest = setInterval(function(){
-			        //		$plex_check_refresh.load('assets/php/plex_check_ajax.php');
-			        //	}, 10000); // at 3, 5 seconds python was crashing.
-
 			        	var refreshIdfastest = setInterval(function(){
 			            	$system_load_refresh.load('assets/php/system_load_ajax.php');
 			        	}, 5000); // 5 seconds
@@ -185,64 +167,11 @@
 			        	var refreshIdslow = setInterval(function(){
 			            	$disk_space_refresh.load('assets/php/disk_space_ajax.php');
 			            	$system_ram_refresh.load('assets/php/system_ram_ajax.php')
-			          //  	$other_disks_refresh.load("assets/php/other_disks_ajax.php");
-			          //	$plex_movie_stats_refresh.load("assets/php/plex_movie_stats_ajax.php")
 			        	}, 300000); // 5 minutes
 
 			        	var refreshtopleft = setInterval(function(){
 			            	$left_column_top_refresh.load('assets/php/left_column_top_ajax.php');
 			        	}, 300000); // 5 minutes
-
-			        //	var refreshlongest = setInterval(function(){
-			        //    	$plex_movie_stats_refresh.load("assets/php/plex_movie_stats_ajax.php");
-			        //	}, 3600000); // 1 hour
-
-			        	// Load these sections only if Plex has changed states
-			        //	var theResource = "assets/misc/plexcheckfile2.txt";
- 
- 				/*var refreshconditional = setInterval(function(){
-				          if(localStorage["resourcemodified"]) {
-				               $.ajax({
-				                    url:theResource,
-				                    type:"head",
-				                    success:function(res,code,xhr) {
-				                         console.log("Checking Plex XML "+ localStorage["resourcemodified"] + " to "+ xhr.getResponseHeader("Last-Modified"))
-				                         if(localStorage["resourcemodified"] != xhr.getResponseHeader("Last-Modified")) getResource();
-				                    }
-				               })
-				 
-				          } else getResource();
-				 
-				          function getResource() {
-				               $.ajax({
-				                    url:theResource,
-				                    type:"get",
-				                    cache:false,
-				                    success:function(res,code,xhr) {
-				                         localStorage["resourcemodified"] = xhr.getResponseHeader("Last-Modified");
-				                         console.log("Updating our cache and refreshing Now Playing divs");
-				                         $left_column_top_refresh.load('assets/php/left_column_top_ajax.php');
-				                         $now_playing_title_refresh.load("assets/php/now_playing_title_ajax.php");
-			        			   $now_playing_refresh.load("assets/php/now_playing_ajax.php");
-				                    }                    
-				               })
-				          }
-				}, 5000); // 5 seconds
-*/
-/*				// Change the size of the now playing div to match the client size every time it's resized
-				function doResizeNowPlaying() {
-					var height = 0;
-					var body = window.document.body;
-					if (window.innerHeight) {
-						height = window.innerHeight;
-					} else if (body.parentElement.clientHeight) {
-						height = body.parentElement.clientHeight;
-					} else if (body && body.clientHeight) {
-						height = body.clientHeight;
-					}
-					now_playing.style.height = ((height - now_playing.offsetTop) + "px");
-					console.log("Div resize complete. New size is: " + height);
-				};*/
 
 				if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 					// some code..
@@ -359,6 +288,5 @@
 			</div>
 		</div>
 		<!-- Invisible php div-->
-		
 		</body>
 </html>
